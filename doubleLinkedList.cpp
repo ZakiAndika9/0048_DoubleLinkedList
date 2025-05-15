f78c59125b9a6c9373f5e79469aba78f9ae8c47f
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-05-15 10:59:33 +0700

    commit 10

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -55,5 +55,10 @@ class DoubleLinkedList
 		{
 			current = current->next;
 		}
+
+		if (current->next != NULL && nim == current->next->noMhs)
+		{
+			cout << "\nDuplicate roll number not allowed" << endl;
+		}
 	}
 };
