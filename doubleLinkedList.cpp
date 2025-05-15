ebac799ceadebcc78475644df2cc1f5d246ef583
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-05-15 10:34:31 +0700

    commit 3

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -5,7 +5,27 @@ using namespace std;
 class Node
 {
 	public:
-	int nimMhs;
+	int noMhs;
 	Node *next;
 	Node *prev;
+};
+
+class DoubleLinkedList
+{
+	private:
+	Node *START;
+
+	public:
+	DoubleLinkedList()
+	{
+		START = NULL;
+	}
+
+	void addNode()
+	{
+		int nim;
+		string nm;
+		cout << "\nEnter the roll number of the student";
+		cin >> nim;
+	}
 };
